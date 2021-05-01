88c9b0e4052cca25d60e4aef25b2ced1fd7e74e0
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-02 00:08:02 +1000

    changed pic, linked css, added JS but it isn't functioning yet

---
+++ index.js
@@ -0,0 +1,19 @@
+const nav = document.getElementById('nav')
+const openArrow = document.getElementById('open-arrow')
+const closeArrow = document.getElementById('close-nav')
+const container = document.getElementById('container')
+
+openArrow.addEventListener('click', toggle)
+closeArrow.addEventListener('click', toggle)
+
+function toggle() {
+   nav.classList.toggle('active')
+}
+
+container.addEventListener('click', close)
+
+function close(event) {
+  if (!event.target.classList.contains("material-icons-outlined")) {
+      nav.classList.remove('active')
+  }
+}
