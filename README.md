e965530b4a104219758544d3402cc8d34f9584fd
Kayne Sheenan <kaynesheenan+git@gmail.com>
2021-05-02 15:19:23 +1000

    added readme info, fixed up majority of CSS and JS, although JS is not working. Working on fixing that

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a drop-down nav project for Scrimba's weekly coding challenge. 
+https://scrimba.com/scrim/coc4049798fe3b425a0c4354a 
+
+You can see the project live here:
+https://zenidith.github.io/dropdownnav/ 
+
+Enjoy!
+
--- index.js
+++ index.js
@@ -7,13 +7,18 @@ openArrow.addEventListener('click', toggle)
 closeArrow.addEventListener('click', toggle)
 
 function toggle() {
-   nav.classList.toggle('active')
+   nav.classList.toggle('active')  
+   container.classList.toggle('inactive')
 }
+
+
 
 container.addEventListener('click', close)
 
 function close(event) {
   if (!event.target.classList.contains("material-icons-outlined")) {
       nav.classList.remove('active')
+      container.classList.remove('inactive')
+
   }
 }
